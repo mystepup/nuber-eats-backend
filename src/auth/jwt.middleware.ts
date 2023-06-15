e16420f5224c33cbd0fed0c469c7b2afb016eca9
cain <bys20411@gmail.com>
2023-06-16 01:24:13 +0900

    feat: middleware error, token 파싱로직 변경

--- src/auth/jwt.middleware.ts
+++ src/auth/jwt.middleware.ts
@@ -1,4 +1,8 @@
-import { Injectable, NestMiddleware } from "@nestjs/common";
+import {
+    BadRequestException,
+    Injectable,
+    NestMiddleware,
+} from "@nestjs/common";
 import { NextFunction, Request, Response } from "express";
 import { UserService } from "@/src/users/user.service";
 import { JwtService } from "@nestjs/jwt";
@@ -10,17 +14,29 @@ export class JwtMiddleware implements NestMiddleware {
         private readonly jwtService: JwtService,
     ) {}
     async use(req: Request, res: Response, next: NextFunction) {
-        const token = req.headers["x-jwt"];
-        try {
-            const decoded = this.jwtService.verify(token.toString());
-            if (typeof decoded === "object" && decoded.hasOwnProperty("id")) {
-                const { password, ...userWithoutPassword } =
-                    await this.userService.findById(decoded.id);
-                req["user"] = userWithoutPassword;
-            }
+        const token = this.extractTokenFromHeader(req);
+        if (!token) {
             next();
-        } catch (e) {
-            console.error(e);
+        } else {
+            try {
+                const decoded = this.jwtService.verify(token);
+                if (
+                    typeof decoded === "object" &&
+                    decoded.hasOwnProperty("id")
+                ) {
+                    const { password, ...userWithoutPassword } =
+                        await this.userService.findById(decoded.id);
+                    req["user"] = userWithoutPassword;
+                }
+                next();
+            } catch (e) {
+                throw new BadRequestException(e.message);
+            }
         }
+    }
+
+    private extractTokenFromHeader(request: Request): string | undefined {
+        const [type, token] = request.headers.authorization?.split(" ") ?? [];
+        return type === "Bearer" ? token : undefined;
     }
 }
